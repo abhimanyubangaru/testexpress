0f87c826f1e1cbbe31449477e109885a77893bd6
abhimanyubangaru <48572985+abhimanyubangaru@users.noreply.github.com>
2021-01-30 12:55:28 -0800

    Renamed it back to index as there was an issue with "landing" being an unknown reference. Now it is able to receive email address and print it in the terminal

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/landing');
+let index = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', landing.get_landing);
-router.post('/', landing.submit_lead);
+router.get('/', index.get_landing);
+router.post('/', index.submit_lead);
 
 module.exports = router;
